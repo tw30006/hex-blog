35dcc0d7ef173a76f1bcfa9bedcca45af9eeaf15
tw30006 <original.heart.2016@gmail.com>
2025-06-07 18:37:19 +0800

    修改部署後網址的路徑

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/hex-blog/",
+  base: "/",
   server: {
     port: 3000,
     open: true,
